a58eb2ed12d06579b3620aacd0272d80d82bc827
Pedro J. Martínez <pedrojmrt@gmail.com>
2017-01-19 20:36:34 +0100

    Init: added initial state variables

--- src/js/init.js
+++ src/js/init.js
@@ -1,10 +1,11 @@
 define([
-    './core'
-], function( Cinematic ) {
+    "./core",
+    "./var/initializer"
+], function( Cinematic, initializer ) {
     // Initial app state
 
     function _setup() {
-        Cinematic.newRequest('batman', 1, true);
+        Cinematic.newRequest( initializer.initial_title_search, initializer.initial_page, initializer.initial_double_request );
     }
 
     $(document).ready(_setup);
