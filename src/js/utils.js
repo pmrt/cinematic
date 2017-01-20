0bc34f01853ecea5166697c1a8bc0874b20e767c
Pedro J. Martínez <pedrojmrt@gmail.com>
2017-01-20 12:10:50 +0100

    Utils: removed needn't inmediate option from debounce function

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,25 +1,19 @@
 define(function() {
 
-//const
-//    URL_REGEXP = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/;
-//    RegEXP causes an weird unkown error on jQuery.
-
 function isValidPoster( exp ) {
     return exp != 'N/A';
 }
 
-function debounce(func, wait, immediate) {
+function debounce(func, wait) {
     var timeout;
     return function() {
         var context = this, args = arguments;
         var later = function() {
             timeout = null;
-            if (!immediate) func.apply(context, args);
+            func.apply(context, args);
         };
-        var callNow = immediate && !timeout;
         clearTimeout(timeout);
         timeout = setTimeout(later, wait);
-        if (callNow) func.apply(context, args);
     };
 }
 
